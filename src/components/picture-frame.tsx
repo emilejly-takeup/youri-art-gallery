2122b0145be7e19a80b05fc6f3ddd97a199d2c1c
emilejly-takeup <emile.joly@take-up.fr>
2024-12-23 14:26:52 +0100

    Refactor Home and PictureFrame components for improved image loading and state management
    - Converted Home component to use React hooks for random image selection.
    - Updated PictureFrame component to handle image loading state with a loading animation.
    - Enhanced styling for PictureFrame to improve responsiveness and visual feedback during image loading.

--- src/components/picture-frame.tsx
+++ src/components/picture-frame.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState } from "react";
 
 type PictureFrameProps = {
     imageName: string;
@@ -12,26 +15,34 @@ export enum PictureFrameSize {
     LARGE = "large",
 }
 
-const PictureFrame: React.FC<PictureFrameProps> = ({ imageName, variant }) => {
+export const PictureFrame: React.FC<PictureFrameProps> = ({ imageName, variant }) => {
+    const [isLoading, setIsLoading] = useState(true);
     const basePath = process.env.NODE_ENV === "production" ? "/youri-art-gallery" : "";
 
     const sizeClasses = {
-        [PictureFrameSize.MAIN]: "w-3/4 lg:w-2/5 aspect-[4/3]",
-        [PictureFrameSize.SMALL]: "w-32 aspect-square",
-        [PictureFrameSize.MEDIUM]: "w-64 aspect-square",
-        [PictureFrameSize.LARGE]: "w-96 aspect-square",
+        [PictureFrameSize.MAIN]: "w-3/4 lg:w-3/5",
+        [PictureFrameSize.SMALL]: "w-32",
+        [PictureFrameSize.MEDIUM]: "w-64",
+        [PictureFrameSize.LARGE]: "w-96",
     };
 
+    const aspectRatioClass = variant === PictureFrameSize.MAIN ? "pb-[75%]" : "pb-[100%]";
+
     return (
-        <div className={`picture-frame ${sizeClasses[variant]} rounded-xl`}>
-            <img
-                src={`${basePath}/assets/${imageName}`}
-                alt={"Picture"}
-                loading="lazy"
-                className="rounded-xl object-cover object-top w-full h-full"
-            />
+        <div className={`picture-frame ${sizeClasses[variant]} rounded-xl relative`}>
+            <div className={`${aspectRatioClass}`}>
+                <div className={`absolute inset-0 ${isLoading ? "animate-pulse" : ""}`}>
+                    <img
+                        src={`${basePath}/assets/${imageName}`}
+                        alt={"Picture"}
+                        loading="lazy"
+                        className={`rounded-xl object-cover object-top w-full h-full ${
+                            isLoading ? "opacity-0" : "opacity-100"
+                        } transition-opacity duration-300`}
+                        onLoad={() => setIsLoading(false)}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
-
-export default PictureFrame;
